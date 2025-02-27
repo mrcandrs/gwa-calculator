8dea0506201c084e37049b855b6046ed47bc5881
superrjj <quiridojohnharvee@gmail.com>
2025-02-27 22:33:07 +0800

    Added a binding

--- app/src/main/java/com/example/gradecalculator/ComputationGwa.java
+++ app/src/main/java/com/example/gradecalculator/ComputationGwa.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.gradecalculator.databinding.ActivityComputationGwaBinding;
+
 public class ComputationGwa extends AppCompatActivity {
     private TextView txtGwa;
+
+    private ActivityComputationGwaBinding root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
